a21ade70153e3662b76c75f20b2c503e4e3f05b3
fouber <fouber.NET@gmail.com>
2015-10-25 18:58:41 +0800

    微调持续集成发布脚本

--- .build.sh
+++ .build.sh
@@ -4,7 +4,7 @@ if [ -d output ]; then
     echo "➥ Commit files"
     git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG} > /dev/null
     rm -rf ${REPO_SLUG}/*
-    cp -fr output/* ${REPO_SLUG}
+    cp -rf output/* ${REPO_SLUG}
     cd ${REPO_SLUG}
     ls -l
     git config user.email "travis@travis-ci.org"
