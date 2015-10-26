cc19e8cc2970a2bc1782d87d0e4e02416c41366f
fouber <fouber.NET@gmail.com>
2015-10-26 10:29:37 +0800

    微调持续集成发布脚本

--- .deploy.sh
+++ .deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# 使用fis进行构建
+fis release -r ./src -Documpd ./output
+
 if [ -d output ]; then
+    # 提交代码至gh-pages分支
     echo "➥ Commit files"
     git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG} > /dev/null
     rm -rf ${REPO_SLUG}/*
